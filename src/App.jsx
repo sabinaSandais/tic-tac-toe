398c4f001ca65be940f2de912295ebb373ba793a
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-29 16:55:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="main-div"></div>;
+  return (
+    <div className="main-div">
+      <h1>Tic Tac Toe</h1>
+    </div>
+  );
 };
 
 export default App;
