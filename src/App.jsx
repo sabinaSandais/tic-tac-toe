9ba912cfa4018cda61dc375f03e5218861f112b4
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-29 17:09:57 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="main-div">
-      <h1>Tic Tac Toe</h1>
+      <h1 className="game-heading water-background">Tic Tac Toe</h1>
     </div>
   );
 };
