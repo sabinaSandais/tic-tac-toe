f39517e21508ca1d0f256cd45bf47bfc97062ce5
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-29 16:50:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <div className="main-div">App</div>;
+  return <div className="main-div"></div>;
 };
 
 export default App;
