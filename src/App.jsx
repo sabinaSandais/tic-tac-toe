63037294e7023d3dd30795f741cbdd844bf9e24d
sabinaSandais <129327140+sabinaSandais@users.noreply.github.com>
2024-08-31 12:58:52 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Square from "./Square/Square";
 
 const App = () => {
   return (
     <div className="main-div">
-      <h1 className="game-heading water-background">Tic Tac Toe</h1>
+      <div>
+        <h1 className="game-heading water-background">Tic Tac Toe</h1>
+
+        <div className="square-wrapper">
+          <Square />
+        </div>
+      </div>
     </div>
   );
 };
